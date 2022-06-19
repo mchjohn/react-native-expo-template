5de29207430e2cdb3f923d19175f7e88405bcca3
Michel John <michel.santos@dataside.com.br>
2022-06-19 18:41:43 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#222',
+  },
+  text: {
+    fontSize: 24,
+    color: '#fff',
+  }
+})
